ac22e05b35c1abb18075feb5a739059f504990eb
briek <Briek.janssens@student.odisee.be>
2025-06-12 12:22:20 +0200

    Fix: alles gecommit voor volledige push

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ import { app } from './app.js';
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`✅ Joke API live op http://localhost:${PORT}`);
+    console.log(`Joke API live op http://localhost:${PORT}`);
 });
